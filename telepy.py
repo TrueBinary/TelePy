b4673b3cbf76b4aa6c72d8c16a8bb62ace9d5776
TrueBinary <gui15787@gmiail.com>
2019-09-04 23:43:14 -0300

    nothin

--- telepy.py
+++ telepy.py
@@ -40,19 +40,19 @@ def help(update):
 def get(bot,update,args):
 	chat_id= update.message.chat_id
 	print(os.getcwd())
-	while True
-	for i range(0,99)
-		keyword = args[0]
-		response= google_images_download.googleimagesdownload()
-		arguments = {"keywords":keyword,"limit":1,"no_directory":True,"format":"png"}
-		paths = response.download(arguments)
-		bot.send_message(chat_id, text= "wait for some seconds")
-		sleep(0.5)
-		os.system("./rename.sh")
-		dic = os.getcwd() + "/downloads/" + str(i) +".png" 
-		bot.send_photo(chat_id, photo=open(dic,"rb"))
-		os.remove(f"{dic}")
-		break
+	while True:
+		for i range(0,99):
+			keyword = args[0]
+			response= google_images_download.googleimagesdownload()
+			arguments = {"keywords":keyword,"limit":1,"no_directory":True,"format":"png"}
+			paths = response.download(arguments)
+			bot.send_message(chat_id, text= "wait for some seconds")
+			sleep(0.5)
+			os.system("./rename.sh")
+			dic = os.getcwd() + "/downloads/" + str(i) +".png" 
+			bot.send_photo(chat_id, photo=open(dic,"rb"))
+			os.remove(f"{dic}")
+			break
 
 
 def main():
