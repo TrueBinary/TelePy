41f7448d4a01e810bba7e0d5f91edaf80fe4842b
TrueBinary <gui15787@gmiail.com>
2019-09-04 23:15:57 -0300

    nothin

--- telepy.py
+++ telepy.py
@@ -55,7 +55,7 @@ def get(bot,update,args):
 
 
 def main():
-	updater = Updater(TOKEN,use_context=True))
+	updater = Updater(f"{TOKEN}",use_context=True))
 
 	dp = updater.dispatcher
 	dp.add_handler(CommandHandler("start",start))
