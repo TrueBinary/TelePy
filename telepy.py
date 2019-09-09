85b1156a9d68cf583969abc136e80af63e88267f
TrueBinary <gui15787@gmiail.com>
2019-09-09 07:52:53 -0300

    not have nothing to see here

--- telepy.py
+++ telepy.py
@@ -47,18 +47,44 @@ def get(bot,update,args):
 	try:
 		sleep(0.6) 
 		keyword = args[1]
+
 		sufkey = args[2]
 		prekey = args[0]
-		response= google_images_download.googleimagesdownload()
-		arguments = {"keywords":keyword,"suffix_keywords":sufkey,"prefix_keywords":prekey,"limit":1,"no_directory":True,"format":"png","print_urls":True}
-		paths = response.download(arguments)
-		bot.send_message(chat_id, text= "wait for some seconds")
-		sleep(0.5)
-		os.system("./rename.sh")
-		for i in range(0,99):
-			dic = os.getcwd() + "/downloads/" + str(i) +".png" 
-			bot.send_photo(chat_id, photo=open(dic,"rb"))
-			os.remove(f"{dic}")
+		if args = args[1]:
+			response= google_images_download.googleimagesdownload()
+			arguments = {"keywords":keyword,"suffix_keywords":sufkey,"limit":1,"no_directory":True,"format":"png","print_urls":True}
+			paths = response.download(arguments)
+			bot.send_message(chat_id, text= "wait for some seconds")
+			sleep(0.5)
+			os.system("./rename.sh")
+			for i in range(0,99):
+				dic = os.getcwd() + "/downloads/" + str(i) +".png" 
+				bot.send_photo(chat_id, photo=open(dic,"rb"))
+				os.remove(f"{dic}")
+
+		elif args = args[2]:
+			response= google_images_download.googleimagesdownload()
+			arguments = {"keywords":keyword,"suffix_keywords":sufkey,"prefix_keywords":prekey,"limit":1,"no_directory":True,"format":"png","print_urls":True}
+			paths = response.download(arguments)
+			bot.send_message(chat_id, text= "wait for some seconds")
+			sleep(0.5)
+			os.system("./rename.sh")
+			for i in range(0,99):
+				dic = os.getcwd() + "/downloads/" + str(i) +".png" 
+				bot.send_photo(chat_id, photo=open(dic,"rb"))
+
+		elif args = args[0]:
+
+			response= google_images_download.googleimagesdownload()
+			arguments = {"keywords":keyword,"limit":1,"no_directory":True,"format":"png","print_urls":True}
+			paths = response.download(arguments)
+			bot.send_message(chat_id, text= "wait for some seconds")
+			sleep(0.5)
+			os.system("./rename.sh")
+			for i in range(0,99):
+				dic = os.getcwd() + "/downloads/" + str(i) +".png" 
+				bot.send_photo(chat_id, photo=open(dic,"rb"))
+				os.remove(f"{dic}")
 
 	except IndexError as e:
 		bot.send_message(chat_id=update.message.chat_id,text="Error none arguments")
