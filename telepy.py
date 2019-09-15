dddc2c3d61fe8c8d851218bbdcfd68e2346c42a2
TrueBinary <gui15787@gmiail.com>
2019-09-15 14:19:24 -0300

    debugging

--- telepy.py
+++ telepy.py
@@ -17,13 +17,9 @@ MODE = os.getenv("MODE")
 TOKEN = os.getenv("TOKEN")
 if MODE == "dev":
 	def run(updater):
-		chat_id = update.message.chat_id
-		msg_id = update.message.message_id
 		updater.start_polling()
 elif MODE == "proud":
 	def run(updater):
-		chat_id = update.message.chat_id
-		msg_id = update.message.message_id
 		PORT = int(os.environ.get("PORT", "8843"))
 		HEROKU_APP_NAME = os.environ.get("tele-py")
 
@@ -36,6 +32,8 @@ else:
 	sys.exit(1)							  		
 
 def start(bot,update):
+	chat_id = update.message.chat_id
+	msg_id = update.message.message_id
 	botwelcome = """Welcome to the pybobot i'll send to you some random images which you want,
 	why do you not try send /get some shit ?"""
 	bot.send_message(chat_id=chat_id, text=botwelcome, reply_text=message_id)
