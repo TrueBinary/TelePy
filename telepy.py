19a37da0d6caa8cb8874ddb2885e1855368cdb77
TrueBinary <gui15787@gmail.com>
2019-12-19 01:45:15 -0300

    Reddit Crawling

--- telepy.py
+++ telepy.py
@@ -22,7 +22,7 @@ import configparser
 import scrapy
 from scrapy.crawler import CrawlerProcess
 import json
-import time, threading,Queue
+import time, threading,queue
 import logging
 import socket,os,sys
 from time import * 
