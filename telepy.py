34052adca163106ba29db216b23862f802846374
TrueBinary <gui15787@gmail.com>
2019-12-23 19:39:04 -0300

    reddit feed

--- telepy.py
+++ telepy.py
@@ -80,7 +80,7 @@ def send_reddit(bot,update):
 	
 	subreddit = reddit.subreddit("FreeGamesOnSteam")
 	temp = [["vazio"]]
-	with open("temp.txt","r+") as f:
+	with open("temp.txt","a+") as f:
 	 dataold = f.read(json.loads(f))
 		
 		
