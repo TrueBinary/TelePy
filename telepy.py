f7cddd3375db6dfb1eaad84fe95ddea49f30999e
TrueBinary <gui15787@gmail.com>
2020-03-28 17:47:08 -0300

    Update telepy.py

--- telepy.py
+++ telepy.py
@@ -1,7 +1,7 @@
 #lib importantes
 #-*- coding: utf-8 -*-
 
-#Copyright (C) 2008 MrTrue <gui19787@gmail.com>
+#Copyright (C) 2019 MrTrue <gui19787@gmail.com>
 #     This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
 #    the Free Software Foundation, either version 3 of the License, or
